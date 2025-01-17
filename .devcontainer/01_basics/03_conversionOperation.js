7cb9f7fb055cf828c234c5798e72031e7a43af37
Tejas More <132873281+tejas-more09@users.noreply.github.com>
2025-01-17 16:23:28 +0000

    conversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -25,8 +25,53 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*********************OPERATION**********************
+
+let value = 9
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+6);
+// console.log(2-6);
+// console.log(2*6);
+// console.log(2**6);
+// console.log(2/6);
+// console.log(2%6);
+
+
+let str1 ="Hello"
+let str2 =" Tejas"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (2 + 2) * 6 / 3 );
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1 ,num2 ,num3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// Link to Study
+// https://262.ecma-international.org/14.0/index.html?_gl=1*1oh0uat*_ga*MTY2ODExNDM5OC4xNzM3MTI4ODU0*_ga_TDCK4DWEPP*MTczNzEyODg1NC4xLjEuMTczNzEyODg4My4wLjAuMA..#sec-toprimitive
+
+
+
 
 
 
